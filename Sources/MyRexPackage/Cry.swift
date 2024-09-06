60c439290afbaeefc7d36114ec8dd2085c1651da
rex <rex>
2024-09-06 15:04:40 +0800

    add resource

---
+++ Sources/MyRexPackage/Cry.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public class Cry {
+    
+    public init() {}
+    
+    public func hollyCow() {
+        print("hollyCow")
+    }
+}
--- Sources/MyRexPackage/MyRexPackage.swift
+++ Sources/MyRexPackage/MyRexPackage.swift
@@ -1,8 +1,24 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-class MyHelloWorld {
-    
-    func heyYou() {
+import UIKit
+import Foundation
+
+public class MyHelloWorld {
+    public init() {
+        
+    }
+    public func heyYou() {
         print("hey you")
+    }
+    public func loadImage() -> UIImage? {
+        guard let url = Bundle.module.url(forResource: "reximage", withExtension: "png") else {
+            print("Image not found")
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            return UIImage(data: data)
+        } catch {
+            print("Error loading image data: \(error.localizedDescription)")
+            return nil
+        }
     }
 }
